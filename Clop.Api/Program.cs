b5fe1a516e9ac183192ad07987451678f3f6a266
yaroslav <plehanov.kzn2@gmail.com>
2025-01-28 23:55:39 +0300

    Добавил проект Api

---
+++ Clop.Api/Program.cs
@@ -0,0 +1,20 @@
+using Clop.DAL.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDataAccessLayer(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
