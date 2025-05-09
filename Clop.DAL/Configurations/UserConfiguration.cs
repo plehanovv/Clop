5ccee5a8a01441a7dc93fa29fc9bdd2e3889802c
yaroslav <plehanov.kzn2@gmail.com>
2025-05-09 21:16:03 +0300

    Добавил некоторые изменения в репозитории

--- Clop.DAL/Configurations/UserConfiguration.cs
+++ Clop.DAL/Configurations/UserConfiguration.cs
@@ -11,5 +11,16 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(x => x.Id).ValueGeneratedOnAdd();
         builder.Property(x => x.Login).HasMaxLength(100).IsRequired();
         builder.Property(x => x.Password).IsRequired();
+
+        builder.HasData(new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                Login = "Test user1",
+                Password = "hugdfgfdgdfu87gf7$#dfgddfg$g7ggf",
+                CreatedAt = DateTime.UtcNow,
+            }
+        });
     }
 }
