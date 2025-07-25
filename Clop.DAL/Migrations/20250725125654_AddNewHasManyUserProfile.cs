3035a42fb795a17a4ff821f76a24fbadf288bc7e
yaroslav <plehanov.kzn2@gmail.com>
2025-07-25 15:57:36 +0300

    migrations for one to many

---
+++ Clop.DAL/Migrations/20250725125654_AddNewHasManyUserProfile.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clop.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewHasManyUserProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Profile_UserId",
+                table: "Profile");
+
+            migrationBuilder.InsertData(
+                table: "Profile",
+                columns: new[] { "Id", "Address", "Age", "UserId" },
+                values: new object[] { 2L, "Kukushkina 43", 31, 1L });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_UserId",
+                table: "Profile",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Profile_UserId",
+                table: "Profile");
+
+            migrationBuilder.DeleteData(
+                table: "Profile",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_UserId",
+                table: "Profile",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
